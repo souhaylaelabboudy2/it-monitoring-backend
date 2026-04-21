4594309415e027dc11e6c8e2b439bfcd5a1f6207
SouhaylaElAbboudy <souhaylaelabboudy2@gmail.com>
2026-04-21 11:39:58 +0100

    Add NVR monitoring with alerts and validation

--- app/Models/Nvr.php
+++ app/Models/Nvr.php
@@ -6,12 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Nvr extends Model
 {
+    protected $table = 'nvr';
+    public $timestamps = false;
+    
     protected $fillable = [
-    'name',
-    'type',
-    'cameras_count',
-    'status',
-    'disk_usage',
-    'last_sync'
-];
+        'name',
+        'type',
+        'cameras_count',
+        'status',
+        'disk_usage',
+        'last_sync',
+        'last_check'
+    ];
 }
