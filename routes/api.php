1bd0c0acf84586d2f3d59e4926c850c3f4d093a0
SouhaylaElAbboudy <souhaylaelabboudy2@gmail.com>
2026-04-23 09:24:24 +0100

    Add AuthController with login API and Bearer token protection

--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,23 @@ use App\Http\Controllers\AlertController;
 use App\Http\Controllers\IncidentController;
 use App\Http\Controllers\NvrController;
 use App\Http\Controllers\BackupController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/servers', [ServerController::class, 'index']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/servers', [ServerController::class, 'index']);
+    Route::get('/nvr', [NvrController::class, 'index']);
+    Route::get('/alerts', [AlertController::class, 'index']);
+});
+
 Route::post('/update-server', [ServerController::class, 'update']);
 Route::get('/stats', [ServerController::class, 'stats']);
-
-Route::get('/alerts', [AlertController::class, 'index']);
 
 Route::get('/incidents', [IncidentController::class, 'index']);
 Route::post('/incidents', [IncidentController::class, 'store']);
 
-Route::get('/nvr', [NvrController::class, 'index']);
 Route::post('/update-nvr', [NvrController::class, 'update']);
 
 Route::get('/backups', [BackupController::class, 'index']);
